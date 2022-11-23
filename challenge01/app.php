9a1e3f734ce0ac5e582f420f9fa8d29152af559c
AbiezerRamirez <abiezer254@gmail.com>
2022-11-23 10:13:34 +0100

    ♻️ Updated challenge 1

--- challenge01/app.php
+++ challenge01/app.php
@@ -11,19 +11,13 @@ foreach ($file as $line) {
             $userAttribute = explode(':', $value);
             $user[$userAttribute[0]] = trim($userAttribute[1]);
         }
-    } else {
-        if (
-            array_key_exists('usr', $user) &&
-            array_key_exists('eme', $user) &&
-            array_key_exists('psw', $user) &&
-            array_key_exists('age', $user) &&
-            array_key_exists('loc', $user) &&
-            array_key_exists('fll', $user)
-        ) {
-            $count++;
-            $lastUserName = $user['usr'];
-            $user = [];
-        }
+        continue;
+    }
+
+    if (isset($user['usr'], $user['eme'], $user['psw'], $user['age'], $user['loc'], $user['fll'])) {
+        $count++;
+        $lastUserName = $user['usr'];
+        $user = [];
     }
 }
 
