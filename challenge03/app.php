fb96a4f0de003c94df151c426e0093f539151d8a
AbiezerRamirez <abiezer254@gmail.com>
2022-11-23 10:33:52 +0100

    🧑‍💻 Updated variables

--- challenge03/app.php
+++ challenge03/app.php
@@ -5,9 +5,10 @@ $zebra = [
     'oldColor' => '',
     'currentColor' => '',
     'count' => 0,
-    'maxCount' => 0,
-    'finalColor' => ''
 ];
+
+$maxCount = 0;
+$finalColor = '';
 
 foreach ($colors as $color) {
     $zebra['count'] = ($color !== $zebra['currentColor'] && $color === $zebra['oldColor'])
@@ -16,10 +17,10 @@ foreach ($colors as $color) {
     $zebra['oldColor'] = $zebra['currentColor'];
     $zebra['currentColor'] = $color;
 
-    if ($zebra['count'] > $zebra['maxCount']) {
-        $zebra['maxCount'] = $zebra['count'];
-        $zebra['finalColor'] = $color;
+    if ($zebra['count'] > $maxCount) {
+        $maxCount = $zebra['count'];
+        $finalColor = $color;
     }
 }
 
-echo "submit {$zebra['maxCount']}@{$zebra['finalColor']}";
+echo "submit $maxCount@$finalColor";
